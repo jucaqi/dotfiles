8305eeb299f7f5787ef7204284c6d0d896ed42aa
jucaqi <jucashanskideaquino@gmail.com>
2024-10-21 17:46:13 -0500

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ deploy_file() {
 	ln -sf $PWD/$1 $HOME/$TARGET
 }
 
-deploy_file .zshrc
-deploy_file .xinitrc
-deploy_file .gitconfig
-deploy_file .config/bspwm
-deploy_file .config/sxhkd
 deploy_file .config/alacritty
+deploy_file .config/bspwm
+deploy_file .config/nvim
 deploy_file .config/picom
 deploy_file .config/polybar
+deploy_file .config/sxhkd
 deploy_file .config/tmux
-deploy_file .config/nvim
+deploy_file .gitconfig
+deploy_file .xinitrc
+deploy_file .zshrc
