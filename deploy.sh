8855eadd5318ca555a6fa8fc127fa6d00a96b8fb
jucaqi <jucashanskideaquino@gmail.com>
2024-10-21 17:21:10 -0500

    added gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 link_file() {
-    TARGET="$HOME/$2"
-    mkdir -p $(dirname $TARGET)
-    ln -sf "$PWD/$1" "$TARGET"
+	TARGET=$(dirname $1)/
+	if [[ "$TARGET" == "./" ]] then
+		TARGET=""
+	fi
+	ln -sf $PWD/$1 $HOME/$TARGET
 }
 
-link_file zsh/zshrc                 .zshrc
-link_file x11/xinitrc               .xinitrc
-link_file bspwm/bspwmrc             .config/bspwm/bspwmrc
-link_file sxhkd/sxhkdrc             .config/sxhkd/sxhkdrc
-link_file alacritty/alacritty.toml  .config/alacritty/alacritty.toml
-link_file nvim/init.lua             .config/nvim/init.lua
-link_file picom/picom.conf          .config/picom/picom.conf
-link_file polybar/config.ini        .config/polybar/config.ini
-link_file tmux/tmux.conf            .config/tmux/tmux.conf
-
+link_file .zshrc
+link_file .xinitrc
+link_file .config/bspwm
+link_file .config/sxhkd
+link_file .config/alacritty
+link_file .config/picom
+link_file .config/polybar
+link_file .config/tmux
+link_file .config/nvim
